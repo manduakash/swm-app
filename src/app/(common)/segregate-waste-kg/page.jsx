1636b803b9666beea93131d2cc047f328da24ec6
Akash Singh <50767686+manduakash@users.noreply.github.com>
2025-02-26 23:26:10 +0530

    commit

--- src/app/(common)/segregate-waste-kg/page.jsx
+++ src/app/(common)/segregate-waste-kg/page.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { SidebarInset } from "@/components/ui/sidebar"
 import Header from "@/components/header"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
@@ -29,24 +28,3 @@ export default function Page() {
   );
 }
 
-=======
-import { SidebarInset } from "@/components/ui/sidebar"
-import Header from "@/components/header"
-
-export default function Page() {
-
-
-
-  return (
-    (<SidebarInset>
-      <div className="flex flex-col h-screen">
-        <Header />
-        <main className="flex-1 overflow-y-auto p-6 bg-zinc-100">
-         active waste collection agents
-        </main>
-      </div>
-    </SidebarInset>)
-  );
-}
-
->>>>>>> 4c59479e1e5293dab8c37a22bb3647c3a59acb78
--- src/components/sidebars/supervisor-sidebar.jsx
+++ src/components/sidebars/supervisor-sidebar.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use client";
 
 import { usePathname } from "next/navigation";
@@ -54,60 +53,3 @@ export function SupervisorSidebar() {
     </Sidebar>
   );
 }
-=======
-"use client";
-
-import { usePathname } from "next/navigation";
-import { Home, Truck, Users, Calendar, Settings, Award, FileBarChart2Icon } from "lucide-react";
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuItem,
-  SidebarMenuButton,
-} from "@/components/ui/sidebar";
-import Link from "next/link";
-
-const menuItems = [
-  { href: "/supervisor-dashboard", icon: Home, label: "Dashboard" },
-  { href: "/agent-performance-tracking-supervisor", icon: Award, label: "Agents Performance" },
-  { href: "/daily-collection-supervisor-report", icon: FileBarChart2Icon, label: "Collection Report" },
-  { href: "/#", icon: Truck, label: "Collection Status" },
-  { href: "/#", icon: Calendar, label: "Attendance" },
-];
-
-export function SupervisorSidebar() {
-  const pathname = usePathname();
-
-  return (
-    <Sidebar>
-      <SidebarHeader className="flex items-center p-0 border-b-2 border-slate-300">
-        <h2 className="text-lg text-center font-semibold py-4 bg-indigo-100 mx-0 w-full">
-          Supervisor Agent
-        </h2>
-      </SidebarHeader>
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {menuItems.map(({ href, icon: Icon, label }, index) => (
-                <SidebarMenuItem key={index}>
-                  <SidebarMenuButton asChild>
-                    <Link href={href} className={pathname === href ? "text-indigo-600 hover:text-indigo-600 bg-indigo-100 hover:bg-indigo-100 font-bold" : ""}>
-                      <Icon className="mr-2 h-4 w-4" />
-                      {label}
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-    </Sidebar>
-  );
-}
->>>>>>> 4c59479e1e5293dab8c37a22bb3647c3a59acb78
